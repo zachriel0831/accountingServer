f6fefc1af5be2d2db64be1a2c067e6635a25d386
zack ou <tc101fubonzack@gmail.com>
2019-05-17 11:25:06 +0800

    fix server

--- app.js
+++ app.js
@@ -19,8 +19,19 @@ mongoose.connect(`mongodb+srv://zackou:zack0831@myaccounting-xzlks.mongodb.net/t
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
   
-app.use(cors());
-app.options('*', cors());
+var whitelist = ['http://localhost:8083', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // app.use('/',function(req, res, next){
 //   console.log("A new request received at " + Date.now());
